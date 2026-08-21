3e9e61d2573c1902c3eecda665073d31352017e8
Mintesinot getu <148215622+mntc3434@users.noreply.github.com>
2026-08-21 22:28:21 +0300

    Fix import statement formatting in AchievementModal.js

--- src/components/AchievementModal.js
+++ src/components/AchievementModal.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { j
+import { 
   View, Text, StyleSheet, Animated, Dimensions, TouchableOpacity,
 } from 'react-native';
 import { BlurView } from 'expo-blur';
