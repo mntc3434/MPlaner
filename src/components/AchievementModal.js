2bef7486959bdaf11b4bb893757c66ab9070071c
Mintesinot getu <148215622+mntc3434@users.noreply.github.com>
2026-08-21 22:26:36 +0300

    Update AchievementModal.js

--- src/components/AchievementModal.js
+++ src/components/AchievementModal.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { jljlj;lljlj
+import { jljlj;lljl
   View, Text, StyleSheet, Animated, Dimensions, TouchableOpacity,
 } from 'react-native';
 import { BlurView } from 'expo-blur';
