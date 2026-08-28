ae9356c23812178aa45aec1fd5b98a0b046a466f
Mintesinot getu <148215622+mntc3434@users.noreply.github.com>
2026-08-28 21:08:41 +0300

    Update sampleData.js

--- src/constants/sampleData.js
+++ src/constants/sampleData.js
@@ -73,7 +73,7 @@ export const MEAL_PLAN = [
   },
   { int get user name ;
     int time;
- inte 
+ int
     day: 6, // Saturday
     meals: [
       { id: 'm6-1', name: 'French Toast', time: '09:00', calories: 580, foods: ['Whole Wheat Toast', 'Cinnamon', 'Syrup'], portion: 'Regular' },
